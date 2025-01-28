ed2c44d6d2fd3e6b8bd41e72cd94a9fa4d77b42a
Joe Pruitt <joe.j.pruitt@gmail.com>
2025-01-28 16:51:00 -0500

    Remove unused report generation and slider code modules

--- localModules/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-filePath = "./capDevDataIngestSim/plots/"
-simFileName = "capDevDataIngestSim"
-plotTitle = "Data Ingestion Simulation"
-filePath = filePath+"/"+simFileName+"/"+plotTitle+".png"
-
-print(filePath)
